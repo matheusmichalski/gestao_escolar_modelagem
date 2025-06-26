33753293419fa49a8236dde3f31f39848f419f57
luizawestruppdocarmo <luizawestruppdocarmo@gmail.com>
2025-06-26 09:33:03 -0300

    FEAT: negative and "carga horaria" > "cargaria horaria" of "curso" validator

--- escola/models/disciplina.py
+++ escola/models/disciplina.py
@@ -1,15 +1,30 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 import uuid
-
 
 class Disciplina(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
     nome = models.CharField(max_length=100)
-    carga_horaria = models.IntegerField(null=False)
+    carga_horaria = models.IntegerField(
+        null=False,
+        validators=[MinValueValidator(1)]
+    )
     ementa = models.CharField(max_length=500)
     professores = models.ManyToManyField("escola.Professor", related_name="disciplinas")
 
     def __str__(self):
         return self.nome
+
+    def clean(self):
+        super().clean()
+        from escola.models import Curso  
+        cursos = Curso.objects.filter(disciplinas=self)
+        for curso in cursos:
+            if self.carga_horaria > curso.carga_horaria:
+                raise ValidationError(
+                    f"A carga horária da disciplina ({self.carga_horaria}h) "
+                    f"não pode ser maior que a do curso '{curso.nome}' ({curso.carga_horaria}h)."
+                )
