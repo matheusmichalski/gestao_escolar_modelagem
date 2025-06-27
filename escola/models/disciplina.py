edecdb30e3c2fcd5eaf9a219742ab9ddb9318795
matheusmichalski <matheus.michalskifc@gmail.com>
2025-06-26 22:29:13 -0300

    REFACTOR: change order import on Disciplina #11

--- escola/models/disciplina.py
+++ escola/models/disciplina.py
@@ -1,17 +1,16 @@
 from django.db import models
+from escola.models import Curso
 from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
 import uuid
+
 
 class Disciplina(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
     nome = models.CharField(max_length=100)
-    carga_horaria = models.IntegerField(
-        null=False,
-        validators=[MinValueValidator(1)]
-    )
+    carga_horaria = models.IntegerField(null=False, validators=[MinValueValidator(1)])
     ementa = models.CharField(max_length=500)
     professores = models.ManyToManyField("escola.Professor", related_name="disciplinas")
 
@@ -20,7 +19,7 @@ class Disciplina(models.Model):
 
     def clean(self):
         super().clean()
-        from escola.models import Curso  
+
         cursos = Curso.objects.filter(disciplinas=self)
         for curso in cursos:
             if self.carga_horaria > curso.carga_horaria:
